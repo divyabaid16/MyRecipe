604e9016ecf3b771244401abeb643a19b9001e5d
divyabaid16 <divyabaid16@gmail.com>
2018-09-16 17:25:15 +0530

    Initial commit

---
+++ app/src/main/java/com/example/divya/myrecipe/About.java
@@ -0,0 +1,50 @@
+package com.example.divya.myrecipe;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class About extends AppCompatActivity {
+
+    private TextView mTextMessage;
+    private TextView desc;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        desc = (TextView) findViewById(R.id.desc);
+        desc.setText("A mobile application that helps us to get the\n" +
+                "name, recipe and nutrient content of food\n" +
+                "items sorted by nutrients that can be\n" +
+                "prepared on the basis of raw material entered.");
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
